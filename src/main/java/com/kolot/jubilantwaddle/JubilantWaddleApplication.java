a56f895ff967d560802acbd2a7eedcb676c62603
Mykhailo Kolotailo <kolotajlo17@gmail.com>
2023-11-12 15:07:52 +0100

    init

---
+++ src/main/java/com/kolot/jubilantwaddle/JubilantWaddleApplication.java
@@ -0,0 +1,13 @@
+package com.kolot.jubilantwaddle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JubilantWaddleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JubilantWaddleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kolot/jubilantwaddle/JubilantWaddleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kolot.jubilantwaddle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JubilantWaddleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
